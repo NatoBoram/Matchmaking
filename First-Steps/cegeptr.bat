c83b87b6e2629de77219406b2b029cffc0a3f844
Nato Boram <nato.boram@gmail.com>
2015-09-28 10:26:04 -0400

    /wait

--- First-Steps/cegeptr.bat
+++ First-Steps/cegeptr.bat
@@ -1,16 +1,18 @@
 @echo off
+cd /d D:/Nato/
 echo Installation des programmes pour les postes de travail.
 pause
 
+echo Firefox
 start https://www.mozilla.org/en-US/firefox/developer/all/?q=French
 pause
 
 start "" "D:\Nato\Firefox Developer Edition\firefox.exe"
-start /min /wait ping 127.0.0.1 -n 2
-start https://adblockplus.org/
-start https://adblockplus.org/elemhidehelper
-start https://www.libreoffice.org/download/portable-versions/
-start https://www.google.com/get/noto/
+start /wait /min ping 127.0.0.1 -n 2
+start /wait https://adblockplus.org/
+start /wait https://adblockplus.org/elemhidehelper
+start /wait https://www.libreoffice.org/download/portable-versions/
+start /wait https://www.google.com/get/noto/
 pause
 
 start D:\Nato\LibreOfficePortable\Data\fonts
