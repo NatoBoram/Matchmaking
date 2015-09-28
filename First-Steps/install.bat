abca28ef22bc23ae1f06264d9ac49bd2b80cec7b
Nato Boram <nato.boram@gmail.com>
2015-09-28 10:14:32 -0400

    Update install.bat

--- First-Steps/install.bat
+++ First-Steps/install.bat
@@ -1,5 +1,6 @@
 @echo off
 mkdir C:\Nato\
-bitsadmin /transfer Update ^ https://raw.githubusercontent.com/NatoBoram/Update/master/First-Steps/firstrun.bat C:\Nato\install.bat
-start C:\Nato\install.bat
+cd /d C:\Nato\
+bitsadmin /transfer Update ^ https://raw.githubusercontent.com/NatoBoram/Update/master/First-Steps/firstrun.bat install.bat
+start install.bat
 exit
