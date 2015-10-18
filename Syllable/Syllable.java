8ee1b5dc0c9afb1d7e88a0364cb4e63714cd46c7
Inoxydable <InoxydableWrite@users.noreply.github.com>
2015-10-18 17:56:02 -0400

    Update Syllable.java

--- Syllable/Syllable.java
+++ Syllable/Syllable.java
@@ -5,59 +5,34 @@ import java.util.*;
 public class Syllabe {
 
 	// Variables
-	final static char[] VOYELLES = {'a', 'e', 'i', 'o', 'u', 'y'};
-	final static char[] CONSONNES = {'b', 'c', 'd', 'f', 'g', 'h',
-			'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'};
+	static Random r = new Random();
+	static String mot = "";
+
+	// Ajouter une lettre
+	private static boolean add(char[] LETTRES) {
+		if (r.nextInt(2) == 0) { 
+			mot += LETTRES[r.nextInt(LETTRES.length)];
+			return true;
+		}
+		else return false;
+	}
+
+	// Créer une syllabe
+	private static void syllabe() {
+		// Variables
+		final char[] VOYELLES = {'a', 'e', 'i', 'o', 'u', 'y'};
+		final char[] CONSONNES = {'b', 'c', 'd', 'f', 'g', 'h',
+				'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'};
+
+		if (add(CONSONNES)) add(CONSONNES);
+		mot += VOYELLES[r.nextInt(VOYELLES.length)];
+		if (add(VOYELLES)) add(VOYELLES);
+		if (add(CONSONNES)) add(CONSONNES);
+	}
 
 	public static void main(String[] args) {
-		// Variables
-		Random r = new Random();
-		String mot = "";
-
-		// Message
-		int c = Integer.parseInt(JOptionPane.showInputDialog(null, "Combien de syllabes?", "Syllabe 0.1", JOptionPane.PLAIN_MESSAGE));
-
-		// Générer les syllabes
-		for(; c > 0; c--) {
-
-			// Première consonne
-			int x = r.nextInt(2);
-			if (x == 0) {
-				mot += CONSONNES[r.nextInt(CONSONNES.length)];
-
-				// Deuxième consonne
-				x = r.nextInt(2);
-				if (x == 0) mot += CONSONNES[r.nextInt(CONSONNES.length)];
-			}
-
-			// Première voyelle
-			x = 0;
-			if (x == 0) {
-				mot += VOYELLES[r.nextInt(VOYELLES.length)];
-
-				// Deuxième voyelle
-				x = r.nextInt(2);
-				if (x == 0) {
-					mot += VOYELLES[r.nextInt(VOYELLES.length)];
-
-					// Troisième voyelle
-					x = r.nextInt(2);
-					if (x == 0) {
-						mot += VOYELLES[r.nextInt(VOYELLES.length)];
-					}
-				}
-			}
-
-			// Première consonne
-			x = r.nextInt(2);
-			if (x == 0) {
-				mot += CONSONNES[r.nextInt(CONSONNES.length)];
-
-				// Deuxième consonne
-				x = r.nextInt(2);
-				if (x == 0) mot += CONSONNES[r.nextInt(CONSONNES.length)];
-			}
-		}
-		JOptionPane.showMessageDialog(null, "Le mot généré est : " + mot, "Syllabe 0.1", JOptionPane.PLAIN_MESSAGE);
+		syllabe();
+		while(r.nextInt(2) == 0) syllabe();
+		JOptionPane.showMessageDialog(null, "Le mot généré est : " + mot, "Syllabe 0.2", JOptionPane.PLAIN_MESSAGE);
 	}
 }
