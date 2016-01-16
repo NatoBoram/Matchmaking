50a6f959c689fcbf4faa4cc7765a4d5200a21ce6
Nato Boram <nato.boram@gmail.com>
2016-01-16 18:04:26 -0500

    Update

--- First-Steps/care.bat
+++ First-Steps/care.bat
@@ -8,5 +8,9 @@ w32tm /resync
 ipconfig /registerdns
 defrag c: /h /u
 sfc /scannow
+dism /online /cleanup-image /spsuperseded /hidesp
+dism /online /cleanup-image /startcomponentcleanup /resetbase
+dism /online /cleanup-image /scanhealth
+dism /online /cleanup-image /checkhealth
 dism /online /cleanup-image /restorehealth
 shutdown /r
--- First-Steps/firstrun.bat
+++ First-Steps/firstrun.bat
@@ -4,100 +4,107 @@ echo Installation des premiers logiciels sur un nouvel appareil.
 pause
 
 echo Mozilla
-start "" "https://www.mozilla.org/en-US/firefox/developer/all/?q=French"
+start "Firefox" "https://www.mozilla.org/en-US/firefox/all/?q=French, Français"
 pause
 
 echo AdBlockPlus
 start firefox
 start /wait /min ping 127.0.0.1 -n %x%
-start "" "https://adblockplus.org/"
-start "" "https://adblockplus.org/elemhidehelper"
+start "AdBlock Plus" "https://adblockplus.org/"
+start "Element Hide Helper" "https://adblockplus.org/elemhidehelper"
 pause
 
 echo Microsoft
 start firefox
 start /wait /min ping 127.0.0.1 -n %x%
-start "" "https://www.microsoft.com/fr-ca/download/confirmation.aspx?id=35"
-start "" "http://www.microsoft.com/en-us/download/details.aspx?id=30679"
-start "" "http://www.microsoft.com/fr-ca/download/details.aspx?id=44928"
-start "" "http://www.microsoft.com/silverlight/"
+start "DirectX" "https://www.microsoft.com/fr-ca/download/confirmation.aspx?id=35"
+start "Visual C++" "http://www.microsoft.com/en-us/download/details.aspx?id=30679"
+start "Microsoft .NET Framework" "http://www.microsoft.com/fr-ca/download/details.aspx?id=44928"
+start "Silverlight" "http://www.microsoft.com/silverlight/"
 pause
 
 echo Adobe
 start firefox
 start /wait /min ping 127.0.0.1 -n %x%
-start "" "https://get.adobe.com/fr/reader/"
-start "" "https://get.adobe.com/fr/flashplayer/"
-start "" "https://get.adobe.com/fr/air/"
-start "" "https://get.adobe.com/fr/shockwave/"
+start "Adobe Reader" "https://get.adobe.com/fr/reader/"
+start "Adobe Flash Player" "https://get.adobe.com/fr/flashplayer/"
+start "Adobe Air" "https://get.adobe.com/fr/air/"
+start "Adobe Shockwave" "https://get.adobe.com/fr/shockwave/"
 pause
 
 echo Java
 start firefox
 start /wait /min ping 127.0.0.1 -n %x%
-start "" "http://java.com/fr/download/manual.jsp"
+start "Java" "http://java.com/fr/download/manual.jsp"
 pause
 
 echo Apple
 start firefox
 start /wait /min ping 127.0.0.1 -n %x%
-start "" "http://www.apple.com/quicktime/download/"
+start "QuickTime" "http://www.apple.com/quicktime/download/"
 pause
 
 echo Web Plugin
 start firefox
 start /wait /min ping 127.0.0.1 -n %x%
-start "" "https://skypewebexperience.skype.com/content/3-14-1793/plugin_installation.html"
-start "" "https://www.google.com/tools/dlpage/hangoutplugin/"
+start "Skupe Web Plugin" "https://skypewebexperience.skype.com/content/3-14-1793/plugin_installation.html"
+start "Hangouts Web Plugin" "https://www.google.com/tools/dlpage/hangoutplugin/"
 pause
 
 echo Applications
 start firefox
 start /wait /min ping 127.0.0.1 -n %x%
-start "" "http://free.avg.com/ca-fr/free-antivirus-download"
-start "" "https://www.piriform.com/ccleaner/download"
-start "" "http://www.dvdvideosoft.com/"
-start "" "http://www.gimp.org/downloads/"
-start "" "http://www.apple.com/itunes/download/"
-start "" "https://www.magicaljellybean.com/keyfinder/"
-start "" "https://notepad-plus-plus.org/"
-start "" "http://www.libreoffice.org/download/libreoffice-fresh/?type=win-x86_64&lang=fr"
-start "" "http://www.skype.com/fr/download-skype/skype-for-computer/"
-start "" "https://www.teamviewer.com/"
-start "" "http://www.videolan.org/vlc/download-windows.html"
-start "" "http://www.win-rar.com/predownload.html?&Version=64bit"
+start "AVG" "http://free.avg.com/ca-fr/free-antivirus-download"
+start "Audacity" "http://audacityteam.org/download/"
+start "BitTorrent" "http://www.bittorrent.com/lang/fr/downloads/win"
+start "CCleaner" "https://www.piriform.com/ccleaner/download"
+start "DVDVideoSoft" "http://www.dvdvideosoft.com/"
+start "Gimp" "http://www.gimp.org/downloads/"
+start "iTunes" "http://www.apple.com/itunes/download/"
+start "KeyFinder" "https://www.magicaljellybean.com/keyfinder/"
+start "LibreOffice" "http://www.libreoffice.org/download/libreoffice-fresh/?type=win-x86_64&lang=fr"
+start "Notepad++" "https://notepad-plus-plus.org/"
+start "PDF Architect" "http://www.pdfforge.org/pdfarchitect"
+start "PDF Creator" "http://www.pdfforge.org/pdfcreator"
+start "Skype" "http://www.skype.com/fr/download-skype/skype-for-computer/"
+start "TeamViewer" 	"https://www.teamviewer.com/"
+start "VLC Media Player" "http://www.videolan.org/vlc/download-windows.html"
+start "WinRAR" "http://www.win-rar.com/predownload.html?&Version=64bit"
+start "7-Zip" "http://www.7-zip.org/"
 pause
 
 echo Games
 start firefox
 start /wait /min ping 127.0.0.1 -n %x%
-start "" "https://us.battle.net/account/download/"
-start "" "http://www.cheatengine.org/"
-start "" "http://na.leagueoflegends.com/"
-start "" "https://lolupdater.com/?p=download"
-start "" "https://minecraft.net/download"
-start "" "http://store.steampowered.com/about/"
-start "" "http://www.systemrequirementslab.com/cyri"
-start "" "http://www.teamspeak.com/downloads"
+start "Battle.net" "https://us.battle.net/account/download/"
+start "Cheat Engine" "http://www.cheatengine.org/"
+start "GeForce Experience" "http://www.geforce.com/geforce-experience" 
+start "League of Legends" "http://na.leagueoflegends.com/"
+start "LoL Updater" "https://lolupdater.com/?p=download"
+start "Minecraft" "https://minecraft.net/download"
+start "Steam" "http://store.steampowered.com/about/"
+start "SystemRequirementsLab" "http://www.systemrequirementslab.com/cyri"
+start "TeamSpeak" "http://www.teamspeak.com/downloads"
 pause
 
 echo Computer Tools
 start firefox
 start /wait /min ping 127.0.0.1 -n %x%
-start "" "http://www.linuxliveusb.com/fr/download"
-start "" "http://www.speedguide.net/downloads.php"
-start "" "https://www.virtualbox.org/"
-start "" "https://www.wireshark.org/#download"
-start "" "http://www.oracle.com/technetwork/developer-tools/sql-developer/downloads/index.html"
-start "" "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
-start "" "http://www.autohotkey.com/"
-start "" "https://eclipse.org/downloads/"
-start "" "https://filezilla-project.org/download.php"
-start "" "http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html"
-start "" "http://adam.denadai.free.fr/index.php?page=Download"
-start "" "https://desktop.github.com/"
-start "" "http://www.wampserver.com/"
-start "" "https://www.python.org/downloads/"
+start "AutoHotkey" "http://www.autohotkey.com/"
+start "Eclipse" "https://eclipse.org/downloads/"
+start "FileZilla" "https://filezilla-project.org/download.php"
+start "GitHub" "https://desktop.github.com/"
+start "Java SDK" "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
+start "LinuxLive USB Creator" "http://www.linuxliveusb.com/fr/download"
+start "PuTTY" "http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html"
+start "Python" "https://www.python.org/downloads/"
+start "SQL Developer" "http://www.oracle.com/technetwork/developer-tools/sql-developer/downloads/index.html"
+start "Super Macro" "http://adam.denadai.free.fr/index.php?page=Download"
+start "TCP Optimizer" "http://www.speedguide.net/downloads.php"
+start "VirtualBox" "https://www.virtualbox.org/"
+start "WampServer" "http://www.wampserver.com/"
+start "Wireshark" "https://www.wireshark.org/#download"
 pause
 
 del C:\Nato\install.bat
+exit
