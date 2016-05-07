077a7b882a389131169fd022b5bbe0c27941182c
Nato Boram <nato.boram@gmail.com>
2016-05-07 16:35:12 -0400

    Perfect

--- FontInstaller/google.bat
+++ FontInstaller/google.bat
@@ -1,11 +1,5 @@
 @echo off
-
 powershell Invoke-WebRequest -Uri https://codeload.github.com/google/fonts/zip/master -OutFile fonts-master.zip
-
 "C:\Program Files\7-Zip\7z.exe" -aos e fonts-master.zip
-
 move /Y *.ttf Fonts/
-
-del Noto-hinted.zip
-
 exit
--- FontInstaller/noto.bat
+++ FontInstaller/noto.bat
@@ -1,15 +1,9 @@
 @echo off
-
 powershell Invoke-WebRequest -Uri https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip -OutFile Noto-hinted.zip
-
 "C:\Program Files\7-Zip\7z.exe" -aoa e Noto-hinted.zip
-
 move /-Y Noto*.ttf Fonts/
 move /-Y Noto*.otf Fonts/
-
-del Noto-hinted.zip
-del LICENSE_OFL.txt
-
-start fonts\
-
-exit
+move Fonts ..
+cd ..
+start Fonts
+rd /s /q FontInstaller
