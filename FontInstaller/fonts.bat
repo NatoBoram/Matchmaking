974b2d27848700aa8733df04a8c361eac6d21879
Nato Boram <nato.boram@gmail.com>
2016-05-07 16:50:55 -0400

    Changed order

--- FontInstaller/fonts.bat
+++ FontInstaller/fonts.bat
@@ -5,8 +5,10 @@ mkdir FontInstaller
 cd FontInstaller
 mkdir Fonts
 
+powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/NatoBoram/Update/master/FontInstaller/google.bat -OutFile google.bat
 powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/NatoBoram/Update/master/FontInstaller/noto.bat -OutFile noto.bat
+
+start google.bat
 start noto.bat
 
-powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/NatoBoram/Update/master/FontInstaller/google.bat -OutFile google.bat
-start google.bat
+exit
--- FontInstaller/noto.bat
+++ FontInstaller/noto.bat
@@ -3,7 +3,4 @@ powershell Invoke-WebRequest -Uri https://noto-website-2.storage.googleapis.com/
 "C:\Program Files\7-Zip\7z.exe" -aoa e Noto-hinted.zip
 move /-Y Noto*.ttf Fonts/
 move /-Y Noto*.otf Fonts/
-move Fonts ..
-cd ..
-start Fonts
-rd /s /q FontInstaller
+exit
