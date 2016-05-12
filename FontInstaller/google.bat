53e5ffd3df26e292b6c2ee3dee35462643956a6b
Nato Boram <nato.boram@gmail.com>
2016-05-12 19:28:07 -0400

    -*.otf

--- FontInstaller/google.bat
+++ FontInstaller/google.bat
@@ -2,9 +2,9 @@
 powershell Invoke-WebRequest -Uri https://codeload.github.com/google/fonts/zip/master -OutFile fonts-master.zip
 "C:\Program Files\7-Zip\7z.exe" -aos e fonts-master.zip
 move /Y *.ttf Fonts/
-move /Y *.otf Fonts/
 
 move Fonts ..
 cd ..
 start Fonts
 rd /s /q FontInstaller
+exit
