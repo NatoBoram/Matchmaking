dd033592c88fec25a777d4ca35a822a47c158b82
Nato Boram <nato.boram@gmail.com>
2016-05-17 19:32:14 -0400

    While(True)

--- Eldarya/Eldarya.java
+++ Eldarya/Eldarya.java
@@ -6,35 +6,47 @@ import pkUpdater.*;
 /* Changelog
  * 1.0.0 : Updater
  * 1.0.1 : JFrame
+ * 1.0.2 : While(True)
  */
 
 public class Eldarya {
 	final static String PROJECTNAME = "Eldarya";
-	final static String VERSION = "1.0.1";
+	final static String VERSION = "1.0.2";
+	final static String[] OPTIONS = {"+5%", "-5%", "/5-5%"};
+	static double maana = 0;
+
 	public static void main(String[] args) throws IOException {
+
 		// JFrame
 		JFrame menu = new JFrame("Menu");
 		menu.setUndecorated(true);
 		menu.setVisible(true);
 		menu.setLocationRelativeTo(null);
-		
+
 		// Eldarya
-		Updater.update(PROJECTNAME, VERSION);
-		final String[] OPTIONS = {"+5%", "-5%", "/5-5%"};
-		double maana = 0;
-		try {
-			maana = Double.parseDouble(JOptionPane.showInputDialog(menu, "Maana", "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE));
-		} catch (Throwable e) {
-			menu.dispose();
-			System.exit(0);
+		while(true){
+
+			// Input
+			try {
+				maana = Double.parseDouble(JOptionPane.showInputDialog(menu, "Maana", "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE));
+			} catch (Throwable e) {
+				menu.dispose();
+				Updater.update(PROJECTNAME, VERSION);
+				System.exit(0);
+			}
+
+			// Options
+			int option = JOptionPane.showOptionDialog(menu, "Options", "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE, JOptionPane.PLAIN_MESSAGE, null, OPTIONS, OPTIONS);
+
+			// Actions
+			if (option == -1) {
+				menu.dispose();
+				Updater.update(PROJECTNAME, VERSION);
+				System.exit(0);
+			}
+			else if (OPTIONS[option].equals("+5%")) JOptionPane.showMessageDialog(menu, Math.round(maana/100*105), "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE);
+			else if (OPTIONS[option].equals("-5%")) JOptionPane.showMessageDialog(menu, Math.round(maana/105*100), "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE);
+			else if (OPTIONS[option].equals("/5-5%")) JOptionPane.showMessageDialog(menu, Math.round(maana/5/105*100), "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE);
 		}
-		int option = JOptionPane.showOptionDialog(menu, "Options", "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE, JOptionPane.PLAIN_MESSAGE, null, OPTIONS, OPTIONS);
-		if (option == -1) System.exit(0);
-		else if (OPTIONS[option].equals("+5%")) JOptionPane.showMessageDialog(menu, Math.round(maana/100*105), "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE);
-		else if (OPTIONS[option].equals("-5%")) JOptionPane.showMessageDialog(menu, Math.round(maana/105*100), "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE);
-		else if (OPTIONS[option].equals("/5-5%")) JOptionPane.showMessageDialog(menu, Math.round(maana/5/105*100), "Eldarya " + VERSION, JOptionPane.PLAIN_MESSAGE);
-		
-		menu.dispose();
-		System.exit(0);
 	}
 }
