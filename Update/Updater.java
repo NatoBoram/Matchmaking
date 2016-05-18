19db84f7380f91d3ccf180c4106d25d4477da610
Nato Boram <NatoBoram@users.noreply.github.com>
2016-05-18 15:53:43 -0400

    Update Updater.java

--- Update/Updater.java
+++ Update/Updater.java
@@ -1,7 +1,11 @@
-package pkUpdate;
-import java.io.*;
-import java.net.*;
-import javax.swing.*;
+package pkUpdater;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Updater {
 	public static void update(final String PROJECTNAME, final String VERSION) throws IOException {
@@ -9,34 +13,25 @@ public class Updater {
 		// Variables
 		final URL VERSIONURL = new URL("https://raw.githubusercontent.com/NatoBoram/Update/master/" + PROJECTNAME + "/version.txt");
 		final URL DOWNLOADLINK = new URL("https://github.com/NatoBoram/Update/raw/master/" + PROJECTNAME + "/" + PROJECTNAME + ".jar");
-		final String ROOT = "C:/Nato/" + PROJECTNAME + "/";
-		new File("C:/Nato").mkdirs();
-		new File(ROOT).mkdirs();
-		int c = 0;
-
+		
 		try {
-
 			// Get Latest Version
 			InputStream versionStream = VERSIONURL.openStream();
-			c = 0;
 			StringBuffer versionBuffer = new StringBuffer("");
+			int c = 0;
 			while (c != -1) {
 				c = versionStream.read();
 				versionBuffer.append((char) c);
 			}
-
-			// Latest Version
-			final String LATESTVERSION = versionBuffer.toString().substring(versionBuffer.toString().indexOf("[") + 1, versionBuffer.toString().indexOf("]"));
 			versionStream.close();
+			final String LATESTVERSION = versionBuffer.toString().substring(versionBuffer.toString().indexOf("[") + 1, versionBuffer.toString().indexOf("]"));
 
 			// Time to update!
 			if (!VERSION.equals(LATESTVERSION)) {
-				JOptionPane.showMessageDialog(null, "Updating to " + LATESTVERSION + ".", PROJECTNAME + " " + VERSION, JOptionPane.INFORMATION_MESSAGE);
-
 				// Download
 				URLConnection download = DOWNLOADLINK.openConnection();
 				InputStream downloadStream = download.getInputStream();
-				BufferedOutputStream downloadOutput = new BufferedOutputStream(new FileOutputStream(new File(ROOT + PROJECTNAME + ".jar")));
+				OutputStream downloadOutput = new FileOutputStream(new File(System.getProperty("user.dir") + File.separator + PROJECTNAME + ".jar"), false);
 				byte[] downloadBuffer = new byte[32 * 1024];
 				int bytesRead = 0;
 				while ((bytesRead = downloadStream.read(downloadBuffer)) != -1) {
@@ -48,7 +43,6 @@ public class Updater {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "An error occured while preforming update!", "Error", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 }
