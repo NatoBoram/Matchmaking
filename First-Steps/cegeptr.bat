ea50a383f3b3a20f038354f20adb629375a1a0cd
Nato Boram <NatoBoram@users.noreply.github.com>
2016-08-31 11:19:07 -0400

    Skype, Discord, Google Fonts

--- First-Steps/cegeptr.bat
+++ First-Steps/cegeptr.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Installation des programmes pour les postes de travail.
+echo Installation des programmes pour les postes de travail du Cégep de Trois-Rivières.
 pause
 
 start https://www.mozilla.org/en-US/firefox/developer/all/?q=French
@@ -8,9 +8,14 @@ pause
 start "" "D:\Nato\Firefox Developer Edition\firefox.exe"
 start /wait /min ping 127.0.0.1 -n 2
 start /wait https://adblockplus.org/
-start /wait https://adblockplus.org/elemhidehelper
+start /wait https://adblockplus.org/elemhidehelper/
 start /wait https://www.libreoffice.org/download/portable-versions/
 start /wait https://www.google.com/get/noto/
+start /wait https://github.com/google/fonts/
+pause
+
+start /wait https://web.skype.com/
+start /wait https://discordapp.com/
 pause
 
 start D:\Nato\LibreOfficePortable\Data\fonts
