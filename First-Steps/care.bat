d9d05ae173489711e47496ccc45266828c5866db
Nato Boram <NatoBoram@users.noreply.github.com>
2016-09-06 18:47:48 -0400

    Order

--- First-Steps/care.bat
+++ First-Steps/care.bat
@@ -1,25 +1,6 @@
 @echo off
 verify on
 
-echo Click on "Check for problems the next time I start my computer".
-start /wait /min ping 127.0.0.1
-start /wait mdsched
-
-echo Press "Y" then "Enter".
-start /wait /min ping 127.0.0.1
-start /wait chkdsk c: /r
-
-echo Cick on "Start", then "Ok", then "X".
-start /wait /min ping 127.0.0.1
-start /wait sigverif
-
-echo Check everything, then click on "Ok", then "Delete Files".
-start /wait /min ping 127.0.0.1
-start /wait cleanmgr /d c:
-
-echo Congratulation! Now, let this window run in the background. Do not shutdown the computer.
-start /wait /min ping 127.0.0.1
-
 set DNS1=8.8.8.8
 set DNS2=8.8.4.4
 set DNS3=2001:4860:4860::8888
@@ -36,12 +17,32 @@ for /f "tokens=1,2,3*" %%i in ('netsh int show interface') do (
 ipconfig /release
 ipconfig /flushdns
 ipconfig /renew
+cls
+
+echo Click on "Check for problems the next time I start my computer".
+start /wait /min ping 127.0.0.1
+start /wait mdsched
+
+echo Press "Y" then "Enter".
+start /wait /min ping 127.0.0.1
+start /wait chkdsk c: /r
+
+echo Cick on "Start", then "Ok", then "X".
+start /wait /min ping 127.0.0.1
+start /wait sigverif
+
+echo Check everything, then click on "Ok", then "Delete Files".
+start /wait /min ping 127.0.0.1
+start /wait cleanmgr /d c:
 
 mkdir C:\Windows10Upgrade\
 del C:\Windows10Upgrade\Windows10Upgrade.exe
 powershell Start-BitsTransfer -Source http://go.microsoft.com/fwlink/?LinkID=799445 -Destination C:\Windows10Upgrade\Windows10Upgrade.exe
 wuauclt /resetauthorization
-start C:\Windows10Upgrade\Windows10Upgrade.exe
+start /wait C:\Windows10Upgrade\Windows10Upgrade.exe
+
+echo Congratulation! Now, let this window run in the background. Do not shutdown the computer.
+start /wait /min ping 127.0.0.1
 
 defrag /c /h /u
 
