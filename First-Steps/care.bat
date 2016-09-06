e1aa469dd29b94d0d21aaea533f6b809b3d5900b
Nato Boram <NatoBoram@users.noreply.github.com>
2016-09-06 16:34:19 -0400

    Fucking spaces

--- First-Steps/care.bat
+++ First-Steps/care.bat
@@ -25,13 +25,13 @@ set DNS2=8.8.4.4
 set DNS3=2001:4860:4860::8888
 set DNS4=2001:4860:4860::8844
 for /f "tokens=1,2,3*" %%i in ('netsh int show interface') do (
-    if %%i equ Enabled (
+	if %%i equ Enabled (
 		echo "%%l" : %DNS1% + %DNS2% + %DNS3% + %DNS4%
 		netsh int ipv4 set dns name="%%l" static %DNS1% primary validate=no
 		netsh int ipv4 add dns name="%%l" %DNS2% index=2 validate=no
 		netsh int ipv6 set dns name="%%l" static %DNS3% primary validate=no
 		netsh int ipv6 add dns name="%%l" %DNS4% index=2 validate=no
-    )
+	)
 )
 
 ipconfig /release
