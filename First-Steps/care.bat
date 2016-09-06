085efffb99fb8e6237b3742c00a65a52495cf8c0
Nato Boram <NatoBoram@users.noreply.github.com>
2016-09-06 10:27:27 -0400

    User-Friendly

--- First-Steps/care.bat
+++ First-Steps/care.bat
@@ -1,19 +1,32 @@
 @echo off
 verify on
-wuauclt /detectnow /updatenow
-start mdsched
-start sigverif
-start chkdsk c: /b
-start chkdsk d: /b
+
+echo Click on "Check for problems the next time I start my computer".
+start /wait /min ping 127.0.0.1
+start /wait mdsched
+
+echo Press "Y" then "Enter".
+start /wait /min ping 127.0.0.1
+start /wait chkdsk c: /r
+
+echo Cick on "Start", then "Ok", then "X".
+start /wait /min ping 127.0.0.1
+start /wait sigverif
+
+echo Congratulation! Now, let this window run in the background. Do not shutdown the computer.
+start /wait /min ping 127.0.0.1
+
+start wuauclt /resetauthorization /detectnow /updatenow
+start ipconfig /registerdns
+
+defrag /c /h /u
+
 net start w32time
 w32tm /resync
-ipconfig /registerdns
-defrag c: /h /u
-defrag d: /h /u
-dism /online /cleanup-image /checkhealth
-dism /online /cleanup-image /scanhealth
+
 dism /online /cleanup-image /restorehealth
+sfc /scannow
 dism /online /cleanup-image /spsuperseded /hidesp
 dism /online /cleanup-image /startcomponentcleanup /resetbase
-sfc /scannow
+
 shutdown /r
