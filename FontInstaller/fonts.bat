eba3896c9e9b34af70e046e3a03feb3091c776fd
Nato Boram <nato.boram@gmail.com>
2016-09-07 13:25:42 -0400

    BitsTransfer

--- FontInstaller/fonts.bat
+++ FontInstaller/fonts.bat
@@ -1,14 +1,6 @@
-@echo off
-
-cd %USERPROFILE%/Desktop
-mkdir FontInstaller
-cd FontInstaller
-mkdir Fonts
-
-powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/NatoBoram/Update/master/FontInstaller/google.bat -OutFile google.bat
-powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/NatoBoram/Update/master/FontInstaller/noto.bat -OutFile noto.bat
-
-start google.bat
-start noto.bat
-
-exit
+@Echo Off
+CD %UserProfile%/Downloads
+Start PowerShell Start-BitsTransfer -Source https://codeload.github.com/google/fonts/zip/master -Destination Google.zip
+Start PowerShell Start-BitsTransfer -Source https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip -Destination Noto.zip
+Start PowerShell Start-BitsTransfer -Source http://comicneue.com/comic-neue-2.2.zip -Destination ComicNeue.zip
+Exit
--- FontInstaller/noto.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-powershell Invoke-WebRequest -Uri https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip -OutFile Noto-hinted.zip
-"C:\Program Files\7-Zip\7z.exe" -aoa e Noto-hinted.zip
-move /-Y Noto*.ttf Fonts/
-move /-Y Noto*.otf Fonts/
-exit
