f407ba16593079bc9287de655f712dcf96ba62b0
Nato Boram <nato.boram@gmail.com>
2016-09-07 13:32:37 -0400

    More PowerShell

--- FontInstaller/fonts.bat
+++ FontInstaller/fonts.bat
@@ -1,6 +1,6 @@
 @Echo Off
 CD %UserProfile%/Downloads
-Start PowerShell Start-BitsTransfer -Source https://codeload.github.com/google/fonts/zip/master -Destination Google.zip
-Start PowerShell Start-BitsTransfer -Source https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip -Destination Noto.zip
-Start PowerShell Start-BitsTransfer -Source http://comicneue.com/comic-neue-2.2.zip -Destination ComicNeue.zip
+Start PowerShell Start-BitsTransfer -Source https://codeload.github.com/google/fonts/zip/master -Destination Google.zip; Expand-Archive Google.zip -dest Google; Remove-Item Google.zip
+Start PowerShell Start-BitsTransfer -Source https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip -Destination Noto.zip; Expand-Archive Noto.zip -dest Noto; Remove-Item Noto.zip
+Start PowerShell Start-BitsTransfer -Source http://comicneue.com/comic-neue-2.2.zip -Destination ComicNeue.zip; Expand-Archive ComicNeue.zip -dest ComicNeue; Remove-Item ComicNeue.zip
 Exit
