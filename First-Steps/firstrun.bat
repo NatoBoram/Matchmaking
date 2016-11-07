6510525ca32de2b43c99153cdc6179da61aac0a5
Nato Boram <NatoBoram@users.noreply.github.com>
2016-11-07 18:36:49 -0500

    Update firstrun.bat

--- First-Steps/firstrun.bat
+++ First-Steps/firstrun.bat
@@ -1,15 +1,14 @@
 @echo off
-set x=5
 echo Installation des premiers logiciels sur un nouvel appareil.
 pause
 
 echo Mozilla
-start "Firefox" "https://www.mozilla.org/en-US/firefox/all/#fr"
+start "Google Chrome" "https://www.google.ca/chrome/browser/desktop/index.html"
 pause
 
 echo Drivers
-start firefox
-start /wait /min ping 127.0.0.1 -n %x%
+start chrome
+ping 127.0.0.1 > nul
 start "AdBlock Plus" "https://adblockplus.org/"
 start "Element Hide Helper" "https://adblockplus.org/elemhidehelper"
 start "Adobe Air" "https://get.adobe.com/fr/air/"
@@ -20,22 +19,19 @@ start "DirectX" "https://www.microsoft.com/fr-ca/download/confirmation.aspx?id=3
 start "Hangouts Web Plugin" "https://www.google.com/tools/dlpage/hangoutplugin/"
 start "Java" "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
 start "Noto Font" "https://www.google.com/get/noto/"
-start "QuickTime" "http://www.apple.com/quicktime/download/"
-start "Skupe Web Plugin" "https://web.skype.com/fr/"
+start "Google Font" "https://github.com/google/fonts"
 start "Silverlight" "http://www.microsoft.com/silverlight/"
 start "7-Zip" "http://www.7-zip.org/"
 pause
 
 echo Applications
-start firefox
-start /wait /min ping 127.0.0.1 -n %x%
+start chrome
+ping 127.0.0.1 > nul
 start "AVG" "http://free.avg.com/ca-fr/free-antivirus-download"
 start "Audacity" "http://audacityteam.org/download/"
-start "BitTorrent" "http://www.bittorrent.com/lang/fr/downloads/win"
 start "CCleaner" "https://www.piriform.com/ccleaner/download"
 start "Gimp" "http://www.gimp.org/downloads/"
 start "iTunes" "http://www.apple.com/itunes/download/"
-start "KeyFinder" "https://www.magicaljellybean.com/keyfinder/"
 start "LibreOffice" "http://www.libreoffice.org/download/libreoffice-fresh/?type=win-x86_64&lang=fr"
 start "Notepad++" "https://notepad-plus-plus.org/download/"
 start "Skype" "http://www.skype.com/fr/download-skype/skype-for-computer/"
@@ -44,28 +40,27 @@ start "VLC Media Player" "http://www.videolan.org/vlc/download-windows.html"
 pause
 
 echo Games
-start firefox
-start /wait /min ping 127.0.0.1 -n %x%
+start chrome
+ping 127.0.0.1 > nul
 start "Cheat Engine" "http://www.cheatengine.org/"
 start "Discord" "https://discordapp.com/download"
-start "GeForce Experience" "http://www.geforce.com/geforce-experience" 
+start "GeForce Experience" "http://www.geforce.com/geforce-experience"
+start "Razer Synapse" "http://www.razerzone.com/synapse/"
 start "Guild Wars 2" "https://account.arena.net/content"
 start "League of Legends" "http://na.leagueoflegends.com/"
 start "Minecraft" "https://minecraft.net/download"
 start "Steam" "http://store.steampowered.com/about/"
 start "SystemRequirementsLab" "http://www.systemrequirementslab.com/cyri"
-start "TeamSpeak" "http://www.teamspeak.com/downloads"
 pause
 
 echo Computer Tools
-start firefox
-start /wait /min ping 127.0.0.1 -n %x%
+start chrome
+ping 127.0.0.1 > nul
 start "AutoHotkey" "http://www.autohotkey.com/"
 start "Eclipse" "https://eclipse.org/downloads/"
 start "FileZilla" "https://filezilla-project.org/download.php"
 start "GitHub" "https://desktop.github.com/"
 start "Git for Windows" "https://git-for-windows.github.io/"
-start "Java SDK" "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
 start "LinuxLive USB Creator" "http://www.linuxliveusb.com/fr/download"
 start "MinGW" "https://sourceforge.net/projects/mingw/files/latest/download"
 start "PuTTY" "http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html"
