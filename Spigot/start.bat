b2d037b463b20563513323981d123c795bfa67a8
Nato Boram <nato.boram@gmail.com>
2017-07-11 00:34:27 -0400

    Removed -d64

--- Spigot/start.bat
+++ Spigot/start.bat
@@ -3,7 +3,5 @@
 	echo Starting the server in 3 seconds...
 	ping 127.0.0.1 > nul
 	java -jar -d64 spigot.jar nogui
-	ping 127.0.0.1 > nul
 	cls
-	ping 127.0.0.1 > nul
 goto x
--- Spigot/start.sh
+++ Spigot/start.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 while true; do
 	echo Starting the server in 3 seconds...
-	sleep 1
-	echo Starting the server in 2 seconds...
-	sleep 1
-	echo Starting the server in 1 second...
-	sleep 1
-	java -jar -d64 spigot.jar nogui
-	echo Server closed. You can now close the terminal.
 	sleep 3
+	java -jar -d64 spigot.jar nogui
 	clear
 done
